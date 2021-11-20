9c9346d43af844bf32d674c167bb735d6723a4b4
Leonardo Carey <careycoder@gmail.com>
2021-11-20 16:34:57 -0300

    Updates

--- precache-manifest.fc1d5de980c49dbd4245f264edaa20ca.js
+++ precache-manifest.fc1d5de980c49dbd4245f264edaa20ca.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "99a8e2fb1e5af573c8d0859e01bac092",
+    "revision": "8a66870a62fafd02a9e1110c9bc272fa",
     "url": "./index.html"
   },
   {
-    "revision": "ff8f40394e52fadad74b",
-    "url": "./static/js/2.d47cfdc4.chunk.js"
+    "revision": "613a1fffd753e61e9ae7",
+    "url": "./static/js/2.33ca52e1.chunk.js"
   },
   {
     "revision": "f7e623707d9b37a3821ec4c3548a4309",
-    "url": "./static/js/2.d47cfdc4.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.33ca52e1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "1a7a239f166b04488592",
-    "url": "./static/js/main.94b13fd1.chunk.js"
+    "revision": "f729c4b775fba58798b1",
+    "url": "./static/js/main.7eca7535.chunk.js"
   },
   {
     "revision": "e02d67031a05b2ca2ed8",
